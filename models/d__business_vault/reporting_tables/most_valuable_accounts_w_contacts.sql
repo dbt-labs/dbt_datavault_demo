6f6e07a4eb0c522b23aa647fbfae0c4a45973008
Stephan Durry <stephan.durry@fishtownanalytics.com>
2024-04-18 08:00:38 +0000

    new model added

--- models/d__business_vault/reporting_tables/most_valuable_accounts_w_contacts.sql
+++ models/d__business_vault/reporting_tables/most_valuable_accounts_w_contacts.sql
@@ -1,11 +1,23 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-select
-    company_name,
-    concat_ws(' ', first_name, last_name) as contact_full_name,
-    sum(amount) as opportunity_amount
-from {{ ref('opportunities_accounts_contacts_details') }}
-where company_name is not null
-    group by
+with contact_details as (
+
+    select *
+    from {{ ref('opportunities_accounts_contacts_details') }}
+
+), aggragate_data as (
+
+    select
         company_name,
-        concat_ws(' ', first_name, last_name)
+        concat_ws(' ', first_name, last_name) as contact_full_name,
+        sum(amount) as opportunity_amount
+    from contact_details
+    where company_name is not null
+        group by
+            company_name,
+            concat_ws(' ', first_name, last_name)
+
+)
+
+select *
+from aggragate_data
